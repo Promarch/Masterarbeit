3b47c07fa2519a7e53afadf39246a4f8aee25234
Promarch <alexgera98@gmail.com>
2024-06-18 16:18:19 +0200

    Desired Position added, calculated from the desired angle of flexion

--- Cmake_franka/ForceControl.cpp
+++ Cmake_franka/ForceControl.cpp
@@ -25,44 +25,6 @@ void setDefaultBehavior(franka::Robot &robot) {
     robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
 };
 
-// Takes the current robot state and outputs a 6x1 vector: x,y,z,phi_x, phi_y, phi_z
-Eigen::Matrix<double, 6, 1> computeTransformation(const franka::RobotState& robot_state) {
-    // Extract the transformation matrix
-    Eigen::Matrix4d transform_init(robot_state.O_T_EE.data());
-
-    // Extract the position (translation) vector
-    Eigen::Vector3d position_init = transform_init.block<3, 1>(0, 3);
-
-    // Extract the rotation matrix
-    Eigen::Matrix3d rotation_init = transform_init.topLeftCorner(3, 3);
-
-    // Compute the Euler angles
-    double z_rot_init = atan2(rotation_init(1, 0), rotation_init(0, 0));
-    double y_rot_init = atan2(-rotation_init(2, 0), sqrt(pow(rotation_init(2, 1), 2) + pow(rotation_init(2, 2), 2)));
-    double x_rot_init = atan2(rotation_init(2, 1), rotation_init(2, 2));
-
-    // Create the combined 6x1 vector
-    Eigen::Matrix<double, 6, 1> x;
-    x << position_init(0), position_init(1), position_init(2), x_rot_init, y_rot_init, z_rot_init;
-
-    return x;
-}
-
-// Function that takes an orientation (position+rotation) and translates it in the EE coordinates
-Eigen::Matrix<double, 6, 1> OrientationToEE(const franka::RobotState& robot_state, Eigen::Matrix<double, 6,1> vector) {
-
-  // Get the rotation matrix from the translation matrix
-  Eigen::Matrix4d transformation(robot_state.O_T_EE.data());
-  Eigen::Matrix3d rotation = transformation.topLeftCorner(3,3); 
-
-  // Apply the rotation
-  vector.head(3) = rotation * vector.head(3);
-  vector.tail(3) = rotation * vector.tail(3); 
-
-  return vector; 
-
-}
-
 // Macro to convert variable name to string for the function
 #define writeDataToFile(data) writeDataToFileImpl(data, #data ".txt")
 void writeDataToFileImpl(const std::vector<std::array<double, 7>>& data, const std::string& filename) {
@@ -107,8 +69,8 @@ int main() {
     franka::RobotState initial_state = robot.readOnce();
 
       // Damping/Stifness
-    const double translation_stiffness{50.0};
-    const double rotation_stiffness{20.0};
+    const double translation_stiffness{250.0};
+    const double rotation_stiffness{50.0};
     Eigen::MatrixXd K_p = Eigen::MatrixXd::Identity(6, 6);
     Eigen::MatrixXd K_d = Eigen::MatrixXd::Identity(6, 6); 
     K_p.topLeftCorner(3,3) *= translation_stiffness; 
@@ -118,27 +80,31 @@ int main() {
 
       // Variables for the loop
     double time = 0.0;
-    double time_max = 2;
-    double time_acc = 1.5;
+    double time_max = 2.5;
+    double time_acc = 0.8;
     double time_dec = 0.5; // time for decceleration, to stop abrubt braking
     double sampling_interval = 0.1;
     double next_sampling_time = 0;
 
-    // Initial position
+    // Initial orientation
     Eigen::Affine3d transform_init(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
     Eigen::Vector3d position_init(transform_init.translation());
     Eigen::Quaterniond rotation_init(transform_init.linear());
     
-      // Desired relative position
-    Eigen::Matrix<double, 3,1> delta_pos, pos_d;
-    delta_pos = {0.0, -0.2, 0.0};
-    delta_pos = rotation_init * delta_pos; 
-      // Create quaternion to rotate around desired angle
-    double rot_angle = M_PI/6;
-    Eigen::Vector3d axis(1,0,0);
-    Eigen::AngleAxisd angle_axis(rot_angle, axis);
+      // Desired Rotation, created with quaternions
+    double angle_flexion = M_PI/9;
+    double rot_z = M_PI/12;
+    Eigen::Vector3d axis(0,1,0);
+    Eigen::AngleAxisd angle_axis(angle_flexion, axis);
     Eigen::Quaterniond rot_quaternion(angle_axis);
     Eigen::Quaterniond rot_quaternion_rel = rotation_init * rot_quaternion * rotation_init.inverse();
+      // Desired relative position
+    double radius_gelenk = 0.1;
+    Eigen::Matrix<double, 3,1> delta_pos, pos_d;
+    delta_pos = {0.0, 0.0, 0.0};
+    delta_pos(0) = -radius_gelenk * sin(angle_flexion);
+    delta_pos(2) = radius_gelenk - radius_gelenk * cos(angle_flexion);
+    delta_pos = rotation_init * delta_pos; 
     // Compute the desired position and rotation
     pos_d << position_init + delta_pos;
     Eigen::Quaterniond rot_d = rot_quaternion_rel * rotation_init; 
@@ -173,7 +139,7 @@ int main() {
 
         // Compute trajectory between current and desired orientation
       // Positional error
-      Eigen::Matrix<double, 6,1> error; 
+      Eigen::Matrix<double, 6,1> error; error.setZero();
       error.head(3) << pos_d - position; 
       // Rotational error
       if (rot_d.coeffs().dot(rotation.coeffs()) < 0.0) {
@@ -200,7 +166,7 @@ int main() {
       // Stop if time is reached
       if (time >= time_max) {
         std::cout << std::fixed << std::setprecision(3);
-        std::cout << "Time: " << time << std::endl << "Rotation error: " << std::endl << error.tail(3) << std::endl << "Position error: " << distance << std::endl << std::endl; 
+        std::cout << "Time: " << time << std::endl << "Error: " << std::endl << error << std::endl << "Position error: " << distance << std::endl << std::endl; 
         std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
         franka::Torques output = tau_d;
         return franka::MotionFinished(output);
@@ -209,7 +175,7 @@ int main() {
       // Print current wrench, time, and absolute positional error
       if (time >= next_sampling_time) {
         std::cout << std::fixed << std::setprecision(3);
-        std::cout << "Time: " << time << std::endl << "Rotation error: " << std::endl << error.tail(3) << std::endl << "Position error: " << distance << std::endl<< std::endl; 
+        std::cout << "Time: " << time << std::endl << "Error: " << std::endl << error << std::endl << "Position error: " << distance << std::endl<< std::endl; 
         next_sampling_time += sampling_interval;
       }
 
