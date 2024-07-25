6170a6f381f4c7636fd5db5bf477ee3ff6f10a33
Promarch <alexgera98@gmail.com>
2024-07-25 15:03:45 +0200

    Added position based version of FR3 to copy movement data of the robot into the simulation

---
+++ mjctrl/copy_robot_joint.py
@@ -0,0 +1,120 @@
+import mujoco
+import mujoco.viewer
+import numpy as np
+import time
+
+# Integration timestep in seconds. This corresponds to the amount of time the joint
+# velocities will be integrated for to obtain the desired joint positions.
+integration_dt: float = 0.1
+
+# Damping term for the pseudoinverse. This is used to prevent joint velocities from
+# becoming too large when the Jacobian is close to singular.
+damping: float = 1e-4
+
+# Gains for the twist computation. These should be between 0 and 1. 0 means no
+# movement, 1 means move the end-effector to the target in one integration step.
+Kpos: float = 0.95
+Kori: float = 0.95
+
+# Whether to enable gravity compensation.
+gravity_compensation: bool = True
+
+# Simulation timestep in seconds.
+dt: float = 0.001
+
+# Nullspace P gain.
+Kn = np.asarray([10.0, 10.0, 10.0, 10.0, 5.0, 5.0, 5.0])
+
+# Maximum allowable joint velocity in rad/s.
+max_angvel = 0.785
+
+
+def main() -> None:
+    assert mujoco.__version__ >= "3.1.0", "Please upgrade to mujoco 3.1.0 or later."
+
+    # Load the model and data.
+    model = mujoco.MjModel.from_xml_path("franka_fr3/scene_joint_actuator.xml")
+    data = mujoco.MjData(model)
+
+    # Enable gravity compensation. Set to 0.0 to disable.
+    model.body_gravcomp[:] = float(gravity_compensation)
+    model.opt.timestep = dt
+
+    # End-effector site we wish to control.
+    site_name = "attachment_site"
+    site_id = model.site(site_name).id
+
+    # Get the dof and actuator ids for the joints we wish to control. These are copied
+    # from the XML file. Feel free to comment out some joints to see the effect on
+    # the controller.
+    joint_names = [
+        "joint1",
+        "joint2",
+        "joint3",
+        "joint4",
+        "joint5",
+        "joint6",
+        "joint7",
+    ]
+
+    dof_ids = np.array([model.joint(name).id for name in joint_names])
+    actuator_ids = np.array([model.actuator(name).id for name in joint_names])
+
+    # Initial joint configuration saved as a keyframe in the XML file.
+    key_name = "home"
+    key_id = model.key(key_name).id
+
+    # Mocap body we will control with our mouse.
+    mocap_name = "target"
+    mocap_id = model.body(mocap_name).mocapid[0]
+
+    q_test = np.array([1.57079, 0.06, 0.00, -2.11, 0.0, 2.19, 0.84])
+    step_start = 0
+    time_debug = 0.5
+    time_sample = 0.5
+    time_max = 5
+
+    with mujoco.viewer.launch_passive(
+        model=model,
+        data=data,
+        show_left_ui=False,
+        show_right_ui=False,
+    ) as viewer:
+        # Reset the simulation.
+        mujoco.mj_resetDataKeyframe(model, data, key_id)
+
+        # Reset the free camera.
+        mujoco.mjv_defaultFreeCamera(model, viewer.cam)
+
+        # Enable site frame visualization.
+        viewer.opt.frame = mujoco.mjtFrame.mjFRAME_SITE
+
+        while viewer.is_running() and data.time<time_max: 
+            if step_start==0:
+                t_init = time.time()
+
+            step_start = time.time()
+
+#            np.clip(q, *model.jnt_range.T, out=q)
+
+            # Set the control signal and step the simulation.
+
+            data.ctrl[actuator_ids] = q_test[dof_ids]
+            mujoco.mj_step(model, data)
+
+            if (time.time()-t_init)>time_debug:
+                step_current = round(data.time/model.opt.timestep)
+                time_pc = round(1000*(time.time()-t_init))
+                time_data = round(1000*data.time)
+                print(f"Current step: {step_current}; Time wall: {time_pc}ms; Time data:{time_data}")
+
+                time_debug += time_sample
+
+            viewer.sync()
+            time_until_next_step = dt - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+
+
+if __name__ == "__main__":
+    main()
