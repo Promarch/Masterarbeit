cba79b9e0f67656423fcc165cd1dc2d6ac988fb9
Promarch <alexgera98@gmail.com>
2024-09-09 10:13:52 +0200

    Added Plots for the thesis

--- Cmake_franka/plotStuff.py
+++ Cmake_franka/plotStuff.py
@@ -1,9 +1,49 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
-
+#%%
+# Plot Acceleration and deceleration factor
 period_acc = 4
 t = np.linspace(0,period_acc,100)
-y = (1 - np.cos(np.pi * t/period_acc))/2
+y_acc = (1 - np.cos(np.pi * t/period_acc))/2
+y_dec = (1 + np.cos(np.pi * t/period_acc))/2
 
-plt.plot(t,y)
+plt.plot(t,y_acc, label=r"$\alpha_{acc}$")
+plt.plot(t,y_dec, label=r"$\alpha_{dec}$")
+plt.grid(False)
+plt.title("Acceleration factors")
+plt.xlabel("Time [s]")
+plt.legend(loc="center right")
+plt.show()
+#%%
+# Plot Rotation of EE
+def plot_quiver(X,Y,axs):
+    quiver_angle = np.arctan([Y+1]/X)
+    axs.quiver(X,Y,np.cos(quiver_angle), np.sin(quiver_angle))
+    axs.quiver(X,Y,np.cos(quiver_angle+np.pi/2), np.sin(quiver_angle+np.pi/2))
+def plot_text(X,Y,axs):
+    for i in range(len(X)):
+        axs.text(X[i]+0.02, Y[i]-0.05, rf"$x_{i}$") # :[{np.round(X[i],2)}, {np.round(Y[i],2)}]
+
+angle = np.pi/4
+t = np.linspace(angle*2,angle,100)
+r = 1
+x = r*np.cos(t)
+y = r*np.sin(t)-r
+
+fig, axs = plt.subplots(figsize=(4,4))
+axs.plot(x,y)
+plot_quiver(x[[0,-1]],y[[0,-1]],axs)
+# plot_text(x[[0,-1]],y[[0,-1]],axs)
+axs.text(0.04,-r-0.01, rf"$x_0 = x_1$")
+axs.plot(0,-r, 'b+')
+axs.plot([x[-1],0], [y[-1],-r], 'b--')
+axs.text(0.4, -1+0.4-0.05, "r")
+y_lim = [-1.1, 0.1]
+x_lim = [-0.1, 1.1]
+#axs.set_xlim(x_lim)
+#axs.set_ylim(y_lim)
+axs.grid(True)
+axs.set_axis_off()
+axs.axis("equal")
 plt.show()
--- Cmake_franka/plot_diff_null.py
+++ Cmake_franka/plot_diff_null.py
@@ -1,3 +1,4 @@
+#%%
 import glob
 import os
 
@@ -5,26 +6,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#%%
     # Get data
+folder_path = '/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/'
+folder_path = 'C:/Users/Alex Benutzer/Documents/Masterarbeit_gesamt/Masterarbeit/Cmake_franka/build/nullspace_test/'
 # Position without nullspace
-list_of_files_pos_norm = glob.glob('/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/position_data_20240809_170329.txt')
+list_of_files_pos_norm = glob.glob(folder_path+'position_data_20240809_170329.txt')
 filePath_pos_norm = max(list_of_files_pos_norm, key=os.path.getctime)
 pos_norm = np.loadtxt(filePath_pos_norm, delimiter=",")
 # Position with nullspace 
-list_of_files_pos_null = glob.glob('/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/position_data_20240809_170454.txt')
+list_of_files_pos_null = glob.glob(folder_path+'position_data_20240809_170454.txt')
 filePath_pos_null = max(list_of_files_pos_null, key=os.path.getctime)
 pos_null_100 = np.loadtxt(filePath_pos_null, delimiter=",")
 # Position with nullspace 
-list_of_files_pos_null = glob.glob('/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/position_data_20240809_173340.txt')
+list_of_files_pos_null = glob.glob(folder_path+'position_data_20240809_173340.txt')
 filePath_pos_null = max(list_of_files_pos_null, key=os.path.getctime)
 pos_null_400 = np.loadtxt(filePath_pos_null, delimiter=",")
 # Position with nullspace 
-list_of_files_pos_null = glob.glob('/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/position_data_20240809_173915.txt')
+list_of_files_pos_null = glob.glob(folder_path+'position_data_20240809_173915.txt')
 filePath_pos_null = max(list_of_files_pos_null, key=os.path.getctime)
 pos_null_1000 = np.loadtxt(filePath_pos_null, delimiter=",")
 # Position with nullspace 
-list_of_files_pos_null = glob.glob('/home/alexandergerard/Masterarbeit/Cmake_franka/build/nullspace_test/position_data_20240809_180014.txt')
+list_of_files_pos_null = glob.glob(folder_path+'position_data_20240809_180014.txt')
 filePath_pos_null = max(list_of_files_pos_null, key=os.path.getctime)
 pos_null_300 = np.loadtxt(filePath_pos_null, delimiter=",")
 
@@ -35,13 +38,18 @@ distance_null_100 = np.linalg.norm(pos_null_100-pos_null_100[0,:], axis=1)
 distance_null_400 = np.linalg.norm(pos_null_400-pos_null_400[0,:], axis=1)
 distance_null_1000 = np.linalg.norm(pos_null_1000-pos_null_1000[0,:], axis=1)
 distance_null_300 = np.linalg.norm(pos_null_300-pos_null_300[0,:], axis=1)
+x = np.arange(0,len(distance_norm))/1000
 
 fig = plt.figure(figsize=(12,8))
-plt.plot(distance_norm, label = "normal")
-plt.plot(distance_null_100, label = r"$K_n = 100$")
-plt.plot(distance_null_400, label = r"$K_n = 400$")
-plt.plot(distance_null_1000, label = r"$K_n = 1000$")
-plt.plot(distance_null_300, label = r"Ausrichtung")
+factor_mm = 1000
+plt.plot(x, distance_norm*factor_mm, label = "normal")
+plt.plot(x, distance_null_100*factor_mm, label = r"$K_n = 100$")
+plt.plot(x, distance_null_400*factor_mm, label = r"$K_n = 400$")
+plt.plot(np.arange(0,len(distance_null_1000))/1000, distance_null_1000*factor_mm, label = r"$K_n = 1000$")
+# plt.plot(distance_null_300, label = r"Ausrichtung")
 
 plt.legend()
+plt.title("Absolute positional error")
+plt.xlabel("Time [s]")
+plt.ylabel("Error [mm]")
 plt.show()
